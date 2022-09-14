66265de357112886094f6c63bf97cc9148265df8
allanstar-byte <allanmutisya263@gmail.com>
2022-09-14 15:52:56 +0300

    Defining Getter and Setter Methods

--- Object-Orientation.rb
+++ Object-Orientation.rb
@@ -52,5 +52,27 @@ class Dog
   lassie.name = "Lassie"
   
   puts lassie.name
-  puts this_dogs_name
-    
+  
+
+#   ******************************************************************************
+# Defining Getter and Setter Methods
+
+class Dog
+    # setter method
+    def name=(value)
+      @name = value
+    end
+  
+    # getter method
+    def name
+      # access the data from the @name instance variable and return it
+      @name
+    end
+  end
+  
+  odie = Dog.new
+  odie.name = "Odie"
+  
+  # call the Dog#name method
+  puts odie.name
+  
