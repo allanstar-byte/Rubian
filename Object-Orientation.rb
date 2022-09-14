54d67e946879dd0da1f3e592f5cd283233e9f980
allanstar-byte <allanmutisya263@gmail.com>
2022-09-14 15:49:09 +0300

    Implementing Instance Variables

--- Object-Orientation.rb
+++ Object-Orientation.rb
@@ -33,4 +33,24 @@ class Person
         puts "The Person is walking"
     end
 end
+
+# ****************************************************************************
+# Implementing Instance Variables
+
+class Dog
+
+    def name=(dogs_name)
+      @@this_dogs_name = dogs_name
+    end
+  
+    def name
+      @@this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
+  puts this_dogs_name
     
